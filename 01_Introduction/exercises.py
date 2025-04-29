9c4b605130104720105f502f61e57b65af41ed35
Divine8821 <tengazdivine8@gmail.com>
2025-04-30 01:59:46 +0300

    Create exercises.py

---
+++ 01_Introduction/exercises.py
@@ -0,0 +1,14 @@
+#Exercises for the Introduction 
+
+#First Program
+print('Hello World')
+
+#Second Program
+
+name = input('Name: ') #Take user input
+
+#String Methods
+.capitalize()
+.lower()
+.upper()
+.count()
